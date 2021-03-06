3af7b697499b3caa1b5a35524bd7994d0b6cc639
ethanparab <59212466+ethanparab@users.noreply.github.com>
2021-03-05 18:47:55 -0600

    TODO scaffolding

--- GameRunner.java
+++ GameRunner.java
@@ -1,2 +1,17 @@
 public class GameRunner {
+	// TODO create inventory for states and capitals
+
+	// TODO counter
+
+	// TODO constructor
+
+	// TODO greet method
+
+	// TODO request guess method
+
+	// TODO random entry picker
+
+	// TODO array copier
+
+	// TODO array consolidator
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
 	public static void main(String[] args) {
+		// TODO ask for user's name
 
+		// TODO greet user
+
+		// TODO enter game
 	}
 }
